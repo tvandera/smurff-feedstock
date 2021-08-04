ad6475a132bf3d44bb7ff0708d534c3a71351f5d
Tom Vander Aa <vanderaa@imec.be>
2021-08-04 11:36:50 +0200

    WIP: backport from staged-recipes/

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-export CMAKE_ARGS="-DENABLE_MKL=ON -DCMAKE_INSTALL_PREFIX=$PREFIX -DENABLE_MPI=OFF"
+export CMAKE_ARGS="${CMAKE_ARGS} -DENABLE_BLAS=ON -DCMAKE_INSTALL_PREFIX=$PREFIX -DENABLE_MPI=OFF"
 
-# make sure we use CONDA_BUILD_SYSROOT
-# https://github.com/conda/conda-build/issues/3452#issuecomment-47539707
-[[ ${target_platform} == "osx-64" ]] && \
-    CMAKE_ARGS="$CMAKE_ARGS -DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT}"
 
 $PYTHON setup.py install \
     --install-binaries \
